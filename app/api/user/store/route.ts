84582f6fffab48b0fc925b7ada512f83d5c24351
sabbir ahmmed <ahsabbir103@gmail.com>
2023-06-02 17:27:44 +0600

    user store updated

--- app/api/user/store/route.ts
+++ app/api/user/store/route.ts
@@ -1,14 +1,15 @@
 import prisma from '@/app/lib/prisma';
 import bcrypt from "bcryptjs";
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 const bcryptP:any = bcrypt;
 
-export async function POST(request:Request) { 
-   const {firstName, lastName, email, phone, password} = await request.json()
+export async function POST(request:NextRequest) { 
+   const {firstname, lastname, email, phone, password} = await request.json()
 
   // checking if all fields are entered
-  if (!firstName || !email || !email || !password) {
+  console.log(firstname, lastname, email, phone, password)
+  if (!firstname || !lastname || !email || !password) {
     return NextResponse.json({
       ok: false,
       message: "Please provide all the required fields",
@@ -34,9 +35,9 @@ export async function POST(request:Request) {
     // creating user
     const User = await prisma.user.create({
       data: {
-        firstName, 
-        lastName, 
-        email,
+        firstName: firstname,
+        lastName: lastname,
+        email : email,
         password: hashedPassword,
         phone,
         role: "blogger"
