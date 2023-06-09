d186c13f9384d6bbf5111234928f97f924cd233d
sabbir ahmmed <ahsabbir103@gmail.com>
2023-06-09 16:33:46 +0600

    updated db for shop, products, user reg, login, et

--- app/api/shop/all/route.tsx
+++ app/api/shop/all/route.tsx
@@ -1,12 +1,11 @@
-import prisma from '@/app/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
 
 
 export async function GET(request:NextRequest) { 
-   const user:any = await prisma.product.findMany();
+  //  const user:any = await prisma.product.findMany();
     return NextResponse.json({
         ok: false,
-        user
+        message: "this is all products get method"
       });
 }
