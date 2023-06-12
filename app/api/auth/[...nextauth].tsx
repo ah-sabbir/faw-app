3f139073831f6f1f8304a77cb29963413e2a3e43
Sabbir Ahmmed <ahsabbir103@gmail.com>
2023-06-12 22:43:26 +0600

    last update

--- app/api/auth/[...nextauth].tsx
+++ app/api/auth/[...nextauth].tsx
@@ -1,39 +1,52 @@
-// import NextAuth, { NextAuthOptions } from "next-auth";
-// import CredentialsProvider from 'next-auth/providers/credentials';
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
 
+        const res = await fetch("http://localhost:8000/auth/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: credentials?.username,
+            password: credentials?.password,
+          }),
+        });
+        const user = await res.json();
 
-// const authOptions: NextAuthOptions = {
-//   session: {
-//     strategy: "jwt",
-//   },
-//   providers: [
-//     CredentialsProvider({
-//       type: "credentials",
-//       credentials: {
-//       },
-//       async authorize(credentials, req) {
-//         const {email, password} = credentials as {
-//             email: string,
-//             password: string
-//         }
-//         // This function is called when a user tries to sign in with credentials.
-//         // You can use the `credentials` object to get the user's email and password.
-//         // You can also use the `req` object to get information about the request, such as the user's IP address.
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
 
-//         // Check the user's credentials against a database.
-//         // If the user's credentials are correct, return `true`.
-//         // Otherwise, return `false`.
-
-//         // Example:
-//         const user = await prisma?.user.findUnique({where:{email:credentials.email}});
-//         if (user && user.password === credentials.password) {
-//           return true;
-//         } else {
-//           return false;
-//         }
-//       },
-//     }),
-//   ],
-// };
-
-// export default NextAuth(authOptions);
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token, user }) {
+      session.user = token as any;
+      return session;
+    },
+  },
+});
