e8a612491c1cfcc27b4ec6b112bc7ee1cd10348d
sabbir ahmmed <ahsabbir103@gmail.com>
2023-06-23 01:04:27 +0600

    resolve conflict issue

--- app/api/auth/login/route.tsx
+++ app/api/auth/login/route.tsx
@@ -4,7 +4,6 @@
 import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
-<<<<<<< HEAD
 import bcrypt from "bcryptjs";
 
 type Data = {
@@ -14,45 +13,6 @@ type Data = {
 interface UserRequest {
   email: string;
   password: string;
-=======
-const bcryptP:any = bcrypt;
-
-export async function POST(request:Request) { 
-   const data:any = await request.json()
-
-  // checking if all fields are entered
-//   console.log(data);
-  
-    try {
-        const user = await prisma.user.findUnique({
-            where: {
-                email: data?.email,
-            },
-            });
-            console.log(user);
-            if (!user) {
-                return NextResponse.json({
-                ok: false,
-                message: "incorrect user or password",
-                });
-            }
-            const passwordMatch:any = await bcrypt.compare(
-                data?.password || "",
-                user?.password || ""
-            );
-            if (!passwordMatch) {
-                return NextResponse.json({
-                ok: false,
-                message: "incorrect user or password",
-                });
-            }
-
-        return NextResponse.json({ ok: true, user: user });
-
-    } catch (error) {
-        return NextResponse.json({ ok: false, message: error });
-    }
->>>>>>> 06af0c5b28e65a7c3a86983a23466ad83d7c8dab
 }
 
 export async function POST(request:Request) {
