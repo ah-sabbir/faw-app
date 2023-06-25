de8f087c918511a77bb56ce3113bc9e643320f4f
Sabbir Ahmmed <ahsabbir103@gmail.com>
2023-06-25 18:49:45 +0600

    designed navigation manu bar need to make it responsive

--- components/elements/navigation.tsx
+++ components/elements/navigation.tsx
@@ -1,6 +1,14 @@
 "use client";
 import Link from 'next/link';
 import { useState } from 'react';
+import { EmailSVG, InstagramSVG, PintarestSVG, SearchSVG } from '../SVGComponent/elements';
+
+// import all components from SVGComponent folder and SVG object
+// import * as SVG from '@/SVGComponent/elements';
+// import { EmailSVG } from '@/components/SVGComponent/elements';
+
+
+
 
 const Navigation = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -8,51 +16,53 @@ const Navigation = () => {
   const toggleNavbar = () => {
     setIsOpen(!isOpen);
   };
-
+  
   return (
-    <nav className="fixed left-0 right-0 z-31 transition-transform duration-250 border-b-1 border-gray-900 bg-white">
-      <div className="flex items-center w-full md:w-auto">
-        {/* Left Menu */}
-        <ul className={`flex space-x-4 ${isOpen ? 'block' : 'hidden'} md:flex`}>
-          <li className="nav-item"><Link href="">POSTS</Link></li>
-          <li className="nav-item"><Link href="">TRENDS</Link></li>
-          <li className="nav-item"><Link href="">SHOP</Link></li>
-        </ul>
+    <div className="font-normal text-base leading-7 font-lato tracking-wider box-border border-b border-gray-900 bg-white fixed left-0 right-0 z-31 transition-transform duration-250">
+    <div className="font-normal text-base leading-7 font-lato tracking-wide box-border w-full mx-auto px-4 max-w-7xl grid justify-center items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-4 pt-6 pb-6 transition duration-250 ease-in-out">
+      <nav className=" tracking-tighter[0.075em] font-normal text-base leading-[2.7692307692rem] font-lato">
+        <ul className="font-normal text-base leading-[2.7692307692rem] font-lato tracking-[.075em] box-border pl-0 flex justify-between">
 
-        {/* Logo */}
-        <div className="flex items-center justify-center w-full md:w-auto">
-          <span className="font-bold text-lg">Fashion Anywhere</span>
+          <li className="font-normal text-base leading-[2.7692307692rem] font-lato tracking-[.075em] box-border relative inline-block w-auto">
+            <Link className="no-underline text-black uppercase font-bold font-lato" href="/posts">Posts</Link>
+          </li>
+          <li className="font-normal text-base leading-[2.7692307692rem] font-lato tracking-[.075em] box-border relative inline-block w-auto">
+            <Link className="no-underline text-black uppercase font-bold font-lato" href="/my-looks">Trends</Link>
+          </li>
+          <li className="font-normal text-base leading-[2.7692307692rem] font-lato tracking-[.075em] box-border relative inline-block w-auto">
+            <Link className="no-underline text-black uppercase font-bold bg-transparent font-lato" href="/shop">Shop</Link>
+          </li>
+        </ul>
+      </nav>
+      
+      <div className="navigation__logo svg font-normal text-base leading-[2.7692307692rem] font-lato tracking-[.075em] box-border max-h-[55px] max-w-[90%] mx-auto relative">
+        <Link href="#"></Link>
+      </div>
+      <div className="font-normal text-base leading-7 font-serif tracking-tighter box-border justify-start text-center">
+        <div className="font-normal text-base leading-7 font-serif tracking-tighter box-border relative inline-block" data-modal="subscribe">
+        <Link className="no-underline text-black uppercase font-bold" href="#modal-subscribe">Subscribe</Link>
         </div>
       </div>
-
-      <div className="flex items-center mt-4 md:mt-0">
-        {/* Toggle Button */}
-        <button
-          className="block md:hidden text-white focus:outline-none"
-          onClick={toggleNavbar}
-        >
-          <svg className="h-6 w-6" viewBox="0 0 24 24" fill="currentColor">
-            <path
-              className={`${isOpen ? 'hidden' : 'block'}`}
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-            <path
-              className={`${isOpen ? 'block' : 'hidden'}`}
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
-
-        {/* Mobile Menu */}
-        <ul className="flex content-between box-border list-disc star-[1em] end-[1em]">
-          <li className="nav-item"><Link href="">Subscribe</Link></li>
-          <li className="nav-item"><Link href="">Instagram</Link></li>
-          <li className="nav-item"><Link href="">Facebook</Link></li>
-          <li className="nav-item"><Link href="">Pinterest</Link></li>
-          <li className="nav-item"><Link href="">LinkedIn</Link></li>
-        </ul>
+    
+      <nav className="font-normal text-base leading-7 font-Lato tracking-tighter box-border flex justify-between mr-3">
+        <Link className="text-base leading-7 font-Lato tracking-tighter box-border bg-transparent text-black no-underline uppercase font-bold touch-manipulation inline-flex" href="https://instagram.com/blaireadiebee/">
+          <InstagramSVG />
+          </Link> 
+          
+          <Link className="text-base leading-7 font-Lato tracking-tighter box-border bg-transparent text-black no-underline uppercase font-bold touch-manipulation inline-flex" href="https://pinterest.com/atlanticpacific/">
+            <PintarestSVG/>
+            </Link> 
+          
+          <Link className="text-base leading-7 font-Lato tracking-tighter box-border bg-transparent text-black no-underline uppercase font-bold touch-manipulation inline-flex" href="#modal-search">
+            <SearchSVG/>
+          </Link> 
+            
+            <Link className="text-base leading-7 font-Lato tracking-tighter box-border bg-transparent text-black no-underline uppercase font-bold touch-manipulation inline-flex" href="mailto:ahsabbir103@gmail.com" >
+              <EmailSVG/>
+            </Link>
+      </nav>
       </div>
-    </nav>
+    </div>
   );
 };
 
