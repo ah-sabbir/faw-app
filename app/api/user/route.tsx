98d01d0f7921e74402346da937c7d19612d6563d
Sabbir Ahmmed <ahsabbir103@gmail.com>
2023-07-29 12:25:40 +0600

    fixed db fetching issues

--- app/api/user/route.tsx
+++ app/api/user/route.tsx
@@ -1,6 +1,8 @@
 import prisma from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
+// import { PrismaClient as prisma } from "@prisma/client";
+
 
 
 export async function GET(request:NextRequest) { 
