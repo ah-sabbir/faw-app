9e82b48b2ceac71f9d0fa17114787cdcdf9f0c0e
Sabbir Ahmmed <ahsabbir103@gmail.com>
2023-10-25 17:08:34 +0600

    solve the error and need to implement image element

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -1,35 +1,87 @@
 'use client'
 
 import EditorJS from "@editorjs/editorjs";
-import Header from "@editorjs/header";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+
 
 interface editorProps {
     updated: boolean;
 }
 
+
+
 const Editor = ({updated}:editorProps) => {
+    const [isMounted, setIsMounted] = useState(false);
+    const ref = useRef<EditorJS>();
+
+    const initializeEditor = async () => {
+        const Editorjs = (await import("@editorjs/editorjs")).default;
+        const Header = (await import("@editorjs/header")).default;
+        // const Image = (await import("@editorjs/image")).default;
+
+        if(!ref.current) {
+            ref.current = new Editorjs({
+                holder: "builder",
+                placeholder: "Let`s write an awesome story!",
+                tools: {
+                    header: Header,
+                    // image: {
+                    //     class: Image,
+                    //     config: {
+                    //         endpoints: {
+                    //             byFile: "http://localhost:3000/api/uploadImage",
+                    //             byUrl: "http://localhost:3000/api/fetchImage",
+                    //         },
+                    //         additionalRequestHeaders: {
+                    //             "Access-Control-Allow-Origin": "*",
+                    //         },
+                    //     },
+                    // },
+                },
+            });
+        }
+    }
 
     useEffect(() => {
-        const editor = new EditorJS({
-            holder:'editorjs',
-            placeholder: 'Let`s write an awesome story!',
-            tools: {
-                header: Header,
-            },
-        });
+        if(typeof window !== "undefined") {
+            setIsMounted(true);
+        }
+    }, [])
 
-        return () => {
-            editor.isReady.then(() => {
-                editor.save();
+    useEffect(() => {
+        const init = async () => {
+            await initializeEditor();
+        }
+        if(isMounted) {
+            init();
+            return () => {
+                if(ref.current){
+                    ref.current.destroy();
+                }
+            };
+        }
+
+    }, [isMounted])
+
+    const save = () =>{
+        if(ref.current){
+            ref.current.save().then((outputData) => {
+                console.log("Article data: ", outputData);
+            }).catch((error) => {
+                console.log("Saving failed: ", error)
             });
-        };
+        }
+    }
 
+    useEffect(() => {
+        if(updated){
+            save();
+        }
     }, [updated])
 
 return (
     <>
-        <div id="editorjs"></div>
+        <div id="builder"></div>
     </>
 )
 }
