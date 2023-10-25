e16de563842f1505f961519f3251a7a77d5ef0d9
Sabbir Ahmmed <ahsabbir103@gmail.com>
2023-10-25 13:21:46 +0600

    empty commit

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -10,6 +10,22 @@ interface editorProps {
 
 const Editor = ({updated}:editorProps) => {
 
+    useEffect(() => {
+        const editor = new EditorJS({
+            holder:'editorjs',
+            placeholder: 'Let`s write an awesome story!',
+            tools: {
+                header: Header,
+            },
+        });
+
+        return () => {
+            editor.isReady.then(() => {
+                editor.save();
+            });
+        };
+
+    }, [updated])
 
 return (
     <>
