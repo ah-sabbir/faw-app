21da350931f06e86f295f3997f6b6c4474ee0506
sabbir ahmmed <ahsabbir103@gmail.com>
2023-12-16 14:57:46 +0600

    resolve image alt issue

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -66,7 +66,7 @@ const RootHeader = () => {
                         }
 			<FaSearch className="h-4 w-4 text-black mx-2 cursor-pointer text-red" onClick={()=>console.log("search icon")}/>
 			<FaFacebookF className="h-4 w-4 text-[#316FF6] mx-2 cursor-pointer hidden md:block lg:block" onClick={()=>console.log("search icon")}/>
-			<Link href="#" className="py-1  hidden md:block lg:block"><Image src="/static/logos/twitter-x-logo.png" width={15} height={15} /></Link>
+			<Link href="#" className="py-1  hidden md:block lg:block"><Image src="/static/logos/twitter-x-logo.png" width={15} height={15} alt="..." /></Link>
 			<FaPinterest className="h-4 w-4 text-red-500 mx-2 cursor-pointer hidden md:block lg:block" onClick={()=>console.log("search icon")}/>
 			
                         
