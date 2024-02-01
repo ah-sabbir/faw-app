e0b8dd7eda72c737e5d26d84f3dd3923f57d8c41
sabbir ahmmed <ahsabbir103@gmail.com>
2024-02-01 10:31:11 +0600

    updated workflow

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,21 +1,21 @@
 import { MetadataRoute } from 'next'
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
-      url: 'https://acme.com',
+      url: 'https://fashionanywhere.shop',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://acme.com/about',
+      url: 'https://fashionanywhere.shop/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://acme.com/blog',
+      url: 'https://fashionanywhere.shop/blog',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
